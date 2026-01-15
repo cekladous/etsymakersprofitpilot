33381d1b5ec7e0cf35eca9a34614ece25c34df0c
cekladous <cekladous@gmail.com>
2026-01-15 05:53:14 +0000

    File changes

--- src/components/quotes/QuoteFormDialog.jsx
+++ src/components/quotes/QuoteFormDialog.jsx
@@ -18,6 +18,7 @@ import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/component
 import { calculateProfit, formatCurrency } from "@/components/shared/profitCalculator";
 import { Switch } from "@/components/ui/switch";
 import ConvertQuoteDialog from "./ConvertQuoteDialog";
+import AIPriceSuggester from "./AIPriceSuggester";
 
 const CURRENCIES = [
   { code: "USD", symbol: "$" },
