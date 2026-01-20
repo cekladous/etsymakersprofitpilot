c876b9e475361bb348efd2655d1c5caefd598014
cekladous <cekladous@gmail.com>
2026-01-20 01:50:18 +0000

    File changes

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useAuth } from "@/components/auth/AuthProvider";
 import { base44 } from "@/api/base44Client";
 import { Link } from "react-router-dom";
-import { createPageUrl } from "./utils";
+import { createPageUrl } from "../utils";
 import { useQuery } from "@tanstack/react-query";
 import {
   Package,
